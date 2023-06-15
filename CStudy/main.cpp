3e8dd87884ee07e655f402a523dca74f5164915c
KeonhoPark <gse04063@gmail.com>
2023-06-15 23:57:19 +0900

    연습문제 7

--- CStudy/main.cpp
+++ CStudy/main.cpp
@@ -99,6 +99,24 @@ void test6(int n) {
 	}
 }
 
+void test7(int n) {
+	if (n < 60) {
+		printf("F");
+	}
+	else if (n >= 60 && n < 70) {
+		printf("D");
+	}
+	else if (n >= 70 && n < 80) {
+		printf("C");
+	}
+	else if (n >= 80 && n < 90) {
+		printf("B");
+	}
+	else{
+		printf("A");
+	}
+}
+
 int main(void) {
 
 	/*test1(1700, 500);
@@ -106,7 +124,8 @@ int main(void) {
 	test3(10);
 	test4();
 	test5(10);*/
-	test6(10);
+	//test6(10);
+	test7(3);
 
 	return 0;
 }
