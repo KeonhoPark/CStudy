82a678a8824cb310cb31eefe8317ccdb8f5214af
KeonhoPark <gse04063@gmail.com>
2023-06-15 22:55:19 +0900

    연습문제 2

--- CStudy/main.cpp
+++ CStudy/main.cpp
@@ -1,15 +1,30 @@
 #include <stdio.h>
 
-int main(void) {
-	int n = 1700;
-	int m = 500;
-
+void test1(int n, int m) {
 	if (n % m != 0) {
 		printf("지급되어야 하는 동전의 수는 %d 입니다.\n", (n / m) + 1);
 	}
 	else {
 		printf("지급되어야 하는 동전의 수는 %d 입니다.\n", n / m);
 	}
+}
+
+void test2(int n) {
+	int total = 0;
+
+	for (int i = 1; i <= n; i++) {
+		if (i % 2 == 1) {
+			total += i;
+		}
+	}
+
+	printf("1부터 n까지의 홀수의 합 = %d", total);
+}
+
+int main(void) {
+
+	test1(1700, 500);
+	test2(10);
 
 	return 0;
 }
