5956a52d93483976bf4e3e6aceeb77688f2879fa
KeonhoPark <gse04063@gmail.com>
2023-06-15 14:29:32 +0900

    C 변수와 자료형

--- CStudy/main.cpp
+++ CStudy/main.cpp
@@ -1,6 +1,20 @@
 #include <stdio.h>
 
 int main(void) {
-	printf("hello world!");
+	printf("%-010d %d %0.2f\n", 10, 23, 3.14f);
+	printf("sizeof(int) : %u\n", sizeof(int));
+	printf("sizeof(long) : %u\n", sizeof(long));
+	printf("sizeof(char) : %u\n", sizeof(char));
+	printf("sizeof('A') : %u\n", sizeof('A'));
+
+	int a = 20 + 5 * 5;
+
+	if (a) {
+		
+	}
+	else {
+
+	}
+
 	return 0;
 }
