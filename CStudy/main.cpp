eb5ea2cd7b83d0245f4512fae60b68972e3c13da
KeonhoPark <gse04063@gmail.com>
2023-06-16 15:09:32 +0900

    1차원 배열

--- CStudy/main.cpp
+++ CStudy/main.cpp
@@ -1,6 +1,8 @@
 #define _CRT_SECURE_NO_WARNINGS 
 #include <stdio.h>
 
+int ary[] = {5, 2, 3, 1, 4, 4, 5, 2};
+
 void test1(int n, int m) {
 	if (n % m != 0) {
 		printf("지급되어야 하는 동전의 수는 %d 입니다.\n", (n / m) + 1);
@@ -118,6 +120,49 @@ void test7(int n) {
 	}
 }
 
+// 함수의 선언
+void func(int, int);
+
+//함수의 정의
+void func(int a, int b) {
+
+}
+
+void print(int* arr, int count) {
+	printf("arr : %p\n", arr);
+	printf("배열크기 : %llu\n", sizeof(arr));
+	printf("배열원소의 수 : %d", count);
+}
+
+int getMax(int* arr_ptr, int count) {
+	int max = -2147483648;
+
+	for (int i = 0; i < count; i++) {
+		if (max < *(arr_ptr + i)) {
+			max = *(arr_ptr + i);
+		}
+	}
+
+	return max;
+
+}
+
+void sort(int* arr_ptr, int count) {
+	int max = getMax(arr_ptr, count);
+
+	int sorted[6] = {0};
+
+	for (int i = 0; i < count; i++) {
+		sorted[*(arr_ptr + i)]++;
+	}
+
+	for (int j = int(sizeof(sorted) / sizeof(sorted[0])); j > 0; j--) {
+		for (int k = 0; k < sorted[j]; k++) {
+			printf("%d\t", j);
+		}
+	}
+}
+
 int main(void) {
 
 	/*test1(1700, 500);
@@ -128,15 +173,42 @@ int main(void) {
 	test6(10);
 	test7(3);*/
 	
-	freopen("C:/새 폴더/새 텍스트 문서.txt", "r", stdin);
-	int a, b, c;
-	scanf(" %d", &a);
-	scanf(" %d", &b);
-	scanf(" %d", &c);
-	scanf("%d %d %d", &a, &b, &c);
-	printf("%d %d %d", a, b, c);
-	getchar();
-	getchar();
+	/*int arr[] = { 1, 2, 3, 4, 5 };
+	size_t arraySize = sizeof(arr);
+	size_t arrayElement = sizeof(arr[0]);
+	size_t count = sizeof(arr) / sizeof(arr[0]);
+
+	printf("배열 전체의 크기 %d\n", arraySize);
+	printf("배열 원소 하나의 크기 %d\n", arrayElement);
+	printf("배열 원소의 수 %d\n", count);
+
+	for (int i = 0; i < count; i++) {
+		printf("%d\t", arr[i]);*/
+
+	/*int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+	printf("arr : %p\n", arr);
+	printf("배열의 크기: %llu\n", sizeof(arr));
+	print(arr, int(sizeof(arr) / sizeof(arr[0])));
+	int max = getMax(arr, int(sizeof(arr) / sizeof(arr[0])));
+	printf("\n배열의 최댓값: %d", max);
+	sort(ary, int(sizeof(ary) / sizeof(ary[0])));*/
+
+	int arr[][3] = { {1, 2, 3}, {4, 5, 6} };
+	int total = 0;
+	size_t row = sizeof(arr) / sizeof(arr[0]);
+	size_t col = sizeof(arr[0]) / sizeof(arr[0][0]);
+	printf("row = %llu\n", row);
+	printf("col = %llu\n", col);
+
+	for (int i = 0; i < row; i++) {
+		for (int j = 0; j < col; j++) {
+			printf("%d", arr[i][j]);
+			total += arr[i][j];
+		}
+		printf("\n");
+	}
+
+	printf("total: %d, avg: %d", total, total / (col * row));
 
 
 	return 0;
