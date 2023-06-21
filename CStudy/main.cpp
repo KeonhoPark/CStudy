7b84a724320a7baea0e8d6290a4579804ec4df7b
KeonhoPark <gse04063@gmail.com>
2023-06-21 23:51:45 +0900

    교재 연습문제(함수)

--- CStudy/main.cpp
+++ CStudy/main.cpp
@@ -1,42 +1,37 @@
 #define _CRT_SECURE_NO_WARNINGS 
 #include<stdio.h>
 #include<stdlib.h>
+typedef enum{True=1, False=0} bool_t;
+
+bool_t isPrime(int);
+
+bool_t isPrime(int num) {
+	if (num == 1 || num == 2) return False;
+
+	for (int i = 2; i < num; i++) {
+		if (num % i == 0) {
+			return False;
+		}
+	}
+	return True;
+}
+
 
 int main(void) {
-	int num = 1;
-
-	do {
-		printf("1. 입력하기\n");
-		printf("2. 검색하기\n");
-		printf("3. 수정하기\n");
-		printf("4. 삭제하기\n");
-		printf("5. 종료하기\n");
-		printf("===================================\n");
-		printf("메뉴입력: ");
-		scanf("%d", &num);
-		switch (num) {
-			case 1:
-				printf("입력하기 작업을 수행합니다.\n");
-				break;
-			case 2:
-				printf("검색하기 작업을 수행합니다.\n");
-				break;
-			case 3:
-				printf("수정하기 작업을 수행합니다.\n");
-				break;
-			case 4:
-				printf("삭제하기 작업을 수행합니다.\n");
-				break;
-			case 0:
-				printf("종료하기 작업을 수행합니다.\n");
-				break;
-			default:
-				printf("잘못 입력하였습니다.\n");
-				break;
+	int num = 0;
+	if (scanf("%d", &num) == 1) {
+		if (isPrime(num) == True) {
+			printf("소수입니다.\n");
 		}
-	} while (num != 0);
-
+		else {
+			printf("소수가 아닙니다.\n");
+		}
+	}
+	else {
+		printf("잘못 입력하였습니다\n");
+	}
 	printf("프로그램을 종료합니다.\n");
+
 	getchar();
 	return 0;
 }
