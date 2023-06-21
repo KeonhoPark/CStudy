bcd61cb9c15fa746b9e1729a74dde1b8c2383eb5
KeonhoPark <gse04063@gmail.com>
2023-06-21 23:44:19 +0900

    교재 연습문제(do while)

--- CStudy/main.cpp
+++ CStudy/main.cpp
@@ -3,33 +3,40 @@
 #include<stdlib.h>
 
 int main(void) {
-	int num = 0;
-	int flag = 0;
+	int num = 1;
 
-	printf("정수값 입력: ");
-	if (scanf("%d", &num) == 1) {
-		if (num == 1 || num == 2) {
-			printf("소수가 아님\n");
+	do {
+		printf("1. 입력하기\n");
+		printf("2. 검색하기\n");
+		printf("3. 수정하기\n");
+		printf("4. 삭제하기\n");
+		printf("5. 종료하기\n");
+		printf("===================================\n");
+		printf("메뉴입력: ");
+		scanf("%d", &num);
+		switch (num) {
+			case 1:
+				printf("입력하기 작업을 수행합니다.\n");
+				break;
+			case 2:
+				printf("검색하기 작업을 수행합니다.\n");
+				break;
+			case 3:
+				printf("수정하기 작업을 수행합니다.\n");
+				break;
+			case 4:
+				printf("삭제하기 작업을 수행합니다.\n");
+				break;
+			case 0:
+				printf("종료하기 작업을 수행합니다.\n");
+				break;
+			default:
+				printf("잘못 입력하였습니다.\n");
+				break;
 		}
-		else {
-			for (int i = 2; i < num; i++) {
-				if (num % i == 0) {
-					printf("소수가 아님\n");
-					flag = 1;
-					break;
-				}
-			}
-			if (flag == 0) {
-				printf("소수\n");
-			}
-		}
-	}
-	else {
-		printf("잘못 입력하였습니다.\n");
-	}
+	} while (num != 0);
 
 	printf("프로그램을 종료합니다.\n");
 	getchar();
-
 	return 0;
 }
